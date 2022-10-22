dd0432203a9d19225718901e0fc82d08fe19e034
KaziTanvir786 <kazitanvirakter@gmail.com>
2022-10-22 19:40:14 +0600

    Warning icons added

--- student-login-page/script.js
+++ student-login-page/script.js
@@ -6,38 +6,38 @@ var emailRegex = /^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/;
 var passRegex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,}$/;
 
 
-function validateEmail(){
-    if(emailFeild.value == ""){
-        emailFeild.style.border="none";
-        emailFeild.style.backgroundColor="white";
-        emailIcon.style.color="grey";
+function validateEmail() {
+    if (emailFeild.value == "") {
+        emailFeild.style.border = "none";
+        emailFeild.style.backgroundColor = "white";
+        emailIcon.style.color = "grey";
     }
-    else if(!emailFeild.value.match(emailRegex)){
-        emailFeild.style.backgroundColor="#b83838";
-        emailFeild.style.color="white";
-        emailIcon.style.color="white";
+    else if (!emailFeild.value.match(emailRegex)) {
+        emailFeild.style.backgroundColor = "#b83838";
+        emailFeild.style.color = "white";
+        emailIcon.style.color = "white";
     }
-    else{
-        emailFeild.style.backgroundColor="#32a345";
-        emailFeild.style.color="white";
-        emailIcon.style.color="white";
+    else {
+        emailFeild.style.backgroundColor = "#32a345";
+        emailFeild.style.color = "white";
+        emailIcon.style.color = "white";
     }
 }
 
-function validatePass(){
-    if(passFeild.value == ""){
-        passFeild.style.border="none";
-        passFeild.style.backgroundColor="white";
-        passIcon.style.color="grey";
+function validatePass() {
+    if (passFeild.value == "") {
+        passFeild.style.border = "none";
+        passFeild.style.backgroundColor = "white";
+        passIcon.style.color = "grey";
     }
-    else if(!passRegex.test(passFeild.value)){
-        passFeild.style.backgroundColor="#b83838";
-        passFeild.style.color="white";
-        passIcon.style.color="white";
+    else if (!passRegex.test(passFeild.value)) {
+        passFeild.style.backgroundColor = "#b83838";
+        passFeild.style.color = "white";
+        passIcon.style.color = "white";
     }
-    else{
-        passFeild.style.backgroundColor="#32a345";
-        passFeild.style.color="white";
-        passIcon.style.color="white";
+    else {
+        passFeild.style.backgroundColor = "#32a345";
+        passFeild.style.color = "white";
+        passIcon.style.color = "white";
     }
 }
