4394c6af5ddbba6966218e8be28837a9a37045bd
KaziTanvir786 <kazitanvirakter@gmail.com>
2022-10-22 20:15:50 +0600

    Icon visibility on email and pass check added

--- student-login-page/script.js
+++ student-login-page/script.js
@@ -2,42 +2,59 @@ var emailFeild = document.getElementById("input-email");
 var passFeild = document.getElementById("input-pass");
 var emailIcon = document.getElementById("email-icon");
 var passIcon = document.getElementById("pass-icon");
+var warnIconEmail = document.getElementById("warn-icon-email");
+var checkIconEmail = document.getElementById("check-icon-email");
+var warnIconPass = document.getElementById("warn-icon-pass");
+var checkIconPass = document.getElementById("check-icon-pass");
+
 var emailRegex = /^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/;
 var passRegex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,}$/;
 
 
 function validateEmail() {
     if (emailFeild.value == "") {
-        emailFeild.style.border = "none";
-        emailFeild.style.backgroundColor = "white";
-        emailIcon.style.color = "grey";
+        // emailFeild.style.border = "none";
+        // emailFeild.style.backgroundColor = "white";
+        // emailIcon.style.color = "grey";
+        warnIconEmail.style.display = "none"
+        checkIconEmail.style.display = "none"
     }
     else if (!emailFeild.value.match(emailRegex)) {
-        emailFeild.style.backgroundColor = "#b83838";
-        emailFeild.style.color = "white";
-        emailIcon.style.color = "white";
+        // emailFeild.style.backgroundColor = "#b83838";
+        // emailFeild.style.color = "white";
+        // emailIcon.style.color = "white";
+        warnIconEmail.style.display = "inline"
+        checkIconEmail.style.display = "none"
     }
     else {
-        emailFeild.style.backgroundColor = "#32a345";
-        emailFeild.style.color = "white";
-        emailIcon.style.color = "white";
+        // emailFeild.style.backgroundColor = "#32a345";
+        // emailFeild.style.color = "white";
+        // emailIcon.style.color = "white";
+        warnIconEmail.style.display = "none"
+        checkIconEmail.style.display = "inline"
     }
 }
 
 function validatePass() {
     if (passFeild.value == "") {
-        passFeild.style.border = "none";
-        passFeild.style.backgroundColor = "white";
-        passIcon.style.color = "grey";
+        // passFeild.style.border = "none";
+        // passFeild.style.backgroundColor = "white";
+        // passIcon.style.color = "grey";
+        warnIconPass.style.display = "none"
+        checkIconPass.style.display = "none"
     }
     else if (!passRegex.test(passFeild.value)) {
-        passFeild.style.backgroundColor = "#b83838";
-        passFeild.style.color = "white";
-        passIcon.style.color = "white";
+        // passFeild.style.backgroundColor = "#b83838";
+        // passFeild.style.color = "white";
+        // passIcon.style.color = "white";
+        warnIconPass.style.display = "inline"
+        checkIconPass.style.display = "none"
     }
     else {
-        passFeild.style.backgroundColor = "#32a345";
-        passFeild.style.color = "white";
-        passIcon.style.color = "white";
+        // passFeild.style.backgroundColor = "#32a345";
+        // passFeild.style.color = "white";
+        // passIcon.style.color = "white";
+        warnIconPass.style.display = "none"
+        checkIconPass.style.display = "inline"
     }
 }
