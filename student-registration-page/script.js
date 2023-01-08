cda33ad5d69b43c15adad3a02e00bafbc2d4cfdc
KaziTanvir786 <kazitanvirakter@egmail.com>
2023-01-08 17:32:46 +0600

    Checker functions are added

--- student-registration-page/script.js
+++ student-registration-page/script.js
@@ -6,6 +6,9 @@ var warnIconPass = document.getElementById("warn-icon-pass");
 var checkIconPass = document.getElementById("check-icon-pass");
 var confirmPass = document.getElementById("input-confirm-pass");
 var inputStudentStatus = document.getElementById("input-student-status");
+var inputID = document.getElementById("input-id");
+var inputDepartment = document.getElementById("input-department");
+var inputSchool = document.getElementById("input-school");
 
 var emailRegex = /^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/;
 
@@ -31,18 +34,7 @@ function validateEmail() {
 }
 
 function validatePass() {
-    if (passFeild.value == "") {
-        passFeild.style.boxShadow = "0px 0px 20px red";
-        return false;
-    }
-    else if (passFeild.value.length > 3) {
-        passFeild.style.boxShadow = "0px 0px 20px green";
-        return true;
-    }
-    else {
-        passFeild.style.boxShadow = "0px 0px 20px red";
-        return false;        
-    }
+    
 }
 function validateConfirmPass() {
     if (passFeild.value == "") {
@@ -70,6 +62,42 @@ function validateStudentStatus() {
     }
     else {
         inputStudentStatus.style.boxShadow = "0px 0px 20px red";
+        return false;
+    }
+}
+
+function validateID(){
+    if (inputID.value == "") {
+        inputID.style.boxShadow = "0px 0px 20px red";
+        return false;
+    }
+    else if (inputID.value.length == 10) {
+        inputID.style.boxShadow = "0px 0px 20px green";
+        return true;
+    }
+    else {
+        inputID.style.boxShadow = "0px 0px 20px red";
+        return false;        
+    }
+}
+
+function validateDepartment(){
+    if(inputDepartment.value == "Department"){
+        inputDepartment.style.boxShadow = "0px 0px 20px red";
+        return true;
+    }
+    else {
+        inputDepartment.style.boxShadow = "0px 0px 20px green";
+        return false;
+    }
+}
+function validateSchool(){
+    if(inputSchool.value == "School"){
+        inputSchool.style.boxShadow = "0px 0px 20px red";
+        return true;
+    }
+    else {
+        inputSchool.style.boxShadow = "0px 0px 20px green";
         return false;
     }
 }
